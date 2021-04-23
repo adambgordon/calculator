4fdfea6fa4e90c00b32c9a31af135c522f2bf723
Adam Gordon <adambaumgordon@gmail.com>
2021-04-22 19:02:49 -0600

    update

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+/* MAIN CODE */
 initCalculator();
 
 
 
-
+/* FUNCTIONS */
 function initCalculator() {
-
-    window.addEventListener('keydown',receiveKey);
+    initButtonGrid();
+    window.addEventListener('keydown',receiveKeyboardInput);
 
 }
+function initButtonGrid() {
+    const buttons = document.querySelectorAll(".button-grid div");
+    buttons.forEach( button => {
+        button.style.gridArea = button.id;
+        button.classList.add("button");
+        button.addEventListener("click",receiveMouseInput)
+    });
+}
 
-function receiveKey(event) {
-    console.log(this.value);
-    console.log(event);
+function receiveKeyboardInput(event) {
+    let id;
+    if(event.shiftKey && event.code === "Digit8") {
+        id = "multiply";
+    } else {
+        element = document.querySelector(`div[data-eventCode="${event.code}"]`);
+        if (element) id = element.id;
+    }
+    if (id) receiveInput(id);
+}
+
+function receiveMouseInput(event) {
+    receiveInput(this.id);
+}
+
+function receiveInput(id) {
+    console.log(id);
+}
+
+function getButtonsList () {
+    return document.querySelectorAll(".button");
 }
 
