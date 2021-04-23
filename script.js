e1c6e6a8523ed0075c11cbeb2482bc9870479dd7
Adam Gordon <adambaumgordon@gmail.com>
2021-04-23 09:30:04 -0600

    update

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 let prev;
 let current;
 let operand;
-const keyDictionary = initKeyDictionary();
+const keys = initKeys();
 initCalculator();
 
 
 
 /* FUNCTIONS */
-function initKeyDictionary() {
+function initKeys() {
     return [
-        {id:	"all-clear",	key:	"a"}, // event.Key === (a || A)
-        {id:	"clear",	    key:	"c"}, // event.Key === (c || C)
-        {id:	"backspace",	key:	"bs"}, // event.key === (Backspace)
+        {id:	"all-clear",	key:	"a",    type: "command"}, // event.Key === (a || A)
+        {id:	"clear",	    key:	"c",    type: "command"}, // event.Key === (c || C)
+        {id:	"backspace",	key:	"bs",    type: "command"}, // event.key === (Backspace)
                     
-        {id:	"zero",	        key:	"0"},
-        {id:	"one",	        key:	"1"},
-        {id:	"two",	        key:	"2"},
-        {id:	"three",    	key:	"3"},
-        {id:	"four",	        key:	"4"},
-        {id:	"five",	        key:	"5"},
-        {id:	"six",          key:	"6"},
-        {id:	"seven",        key:	"7"},
-        {id:	"eight",    	key:	"8"},
-        {id:	"nine",	        key:	"9"},
-        {id:	"decimal",	    key:	"."},
+        {id:	"zero",	        key:	"0",    type: "number"},
+        {id:	"one",	        key:	"1",    type: "number"},
+        {id:	"two",	        key:	"2",    type: "number"},
+        {id:	"three",    	key:	"3",    type: "number"},
+        {id:	"four",	        key:	"4",    type: "number"},
+        {id:	"five",	        key:	"5",    type: "number"},
+        {id:	"six",          key:	"6",    type: "number"},
+        {id:	"seven",        key:	"7",    type: "number"},
+        {id:	"eight",    	key:	"8",    type: "number"},
+        {id:	"nine",	        key:	"9",    type: "number"},
+        {id:	"decimal",	    key:	".",    type: "number"},
                     
-        {id:	"equals",	    key:	"="}, // Event.key === (Enter)
-        {id:	"plus",	        key:	"+"}, // Event.key === (+ || =)
-        {id:	"minus",	    key:	"-"},
-        {id:	"multiply",	    key:	"*"},
-        {id:	"divide",	    key:	"/"},
+        {id:	"equals",	    key:	"=",   type: "operator"}, // Event.key === (Enter)
+        {id:	"plus",	        key:	"+",    type: "operator"}, // Event.key === (+ || =)
+        {id:	"minus",	    key:	"-",    type: "operator"},
+        {id:	"multiply",	    key:	"*",    type: "operator"},
+        {id:	"divide",	    key:	"/",    type: "operator"}
     ]
 }
 
@@ -67,19 +67,20 @@ function receiveKeyboardInput(event) {
             key = "+";
             break;
     }
-    if (keyDictionary.some(element => element.key === key)) {
-        console.log(key);
-    }
+    const index = keys.findIndex(element => {return element.key === key});
+    executeKey(index);
 }
 
 function receiveMouseInput(event) {
-    const index = keyDictionary.findIndex(element => {return element.id === this.id});
-    const key  = keyDictionary[index].key;
-    console.log(key);
+    const index = keys.findIndex(element => {return element.id === this.id});
+    executeKey(index);
 }
 
-function receiveInput(id) {
+function executeKey(index) {
+    if (index === -1) return;
+    console.log(keys[index]);
 }
+
 
 function getButtonsList () {
     return document.querySelectorAll(".button");
